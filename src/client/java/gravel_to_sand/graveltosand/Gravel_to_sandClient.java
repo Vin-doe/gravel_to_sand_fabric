37e1fbddf2733fb7603f1e6fd94b0ebe60de579e
root <root@Vin.localdomain>
2025-10-21 19:09:09 +0100

    Initial setup files.

---
+++ src/client/java/gravel_to_sand/graveltosand/Gravel_to_sandClient.java
@@ -0,0 +1,10 @@
+package gravel_to_sand.graveltosand;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Gravel_to_sandClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
